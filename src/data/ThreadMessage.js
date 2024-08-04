71c61248113fffdd55c8be8f046327727c536399
Bsian <chharry321@gmail.com>
2024-08-04 17:44:13 +0100

    jsdoc fix

--- src/data/ThreadMessage.js
+++ src/data/ThreadMessage.js
@@ -8,7 +8,8 @@ const utils = require("../utils/utils");
  * @property {String} user_name
  * @property {String} body
  * @property {Number} is_anonymous
- * @property {Number} dm_message_id
+ * @property {String} dm_message_id
+ * @property {String} thread_message_id
  * @property {String} created_at
  */
 class ThreadMessage {
